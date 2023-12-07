63730fd0a0cf383a4ba2f0dbc50038779562880c
Yiosy <150297399+Yiosy@users.noreply.github.com>
2023-12-07 20:37:41 +0100

    more basic functions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,32 @@
-add;
-subtract;
-multiply;
-divide;
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function operate(operator, firstArgument, secondArgument) {
+  switch (operator) {
+    case "+":
+      return add(firstArgument, secondArgument);
+      break;
+    case "-":
+      return subtract(firstArgument, secondArgument);
+      break;
+    case "*":
+      return multiply(firstArgument, secondArgument);
+      break;
+    case "/":
+      return divide(firstArgument, secondArgument);
+      break;
+  }
+}
