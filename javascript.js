efa554659c0e98ab2bc379b3ba0973734ca3af5e
Yiosy <150297399+Yiosy@users.noreply.github.com>
2023-12-08 21:14:13 +0100

    equals button now works

--- javascript.js
+++ javascript.js
@@ -33,8 +33,32 @@ function operate(operator, firstArgument, secondArgument) {
 const input = document.querySelector("input");
 const clearButton = document.querySelector(".clearButton");
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const zero = document.getElementById("0");
+const operatorAdd = document.getElementById("add");
+const equals = document.getElementById("equals");
 numbers.forEach((x, i) =>
   x.addEventListener("click", () => (input.value = `${input.value}${i + 1}`))
 );
 
 clearButton.addEventListener("click", () => (input.value = ""));
+zero.addEventListener("click", () => (input.value = input.value + "0"));
+operatorAdd.addEventListener("click", () => {
+  if (firstNumber == null) {
+    firstNumber = Number(input.value);
+    operator = "+";
+    input.value = "";
+  } else if (typeof firstNumber == "number") {
+    secondNumber = Number(input.value);
+    input.value = `${firstNumber}${operator}${secondNumber}`;
+  }
+});
+equals.addEventListener("click", () => {
+  if (typeof firstNumber == "number" && typeof secondNumber == "number") {
+    input.value = operate(operator, firstNumber, secondNumber);
+    firstNumber = Number(input.value);
+  } else if (typeof firstNumber == "number" && secondNumber == null) {
+    secondNumber = Number(input.value);
+    input.value = operate(operator, firstNumber, secondNumber);
+  }
+});
