4df012e1dc59dcd1b9d62be7a4bd90cfc934422d
Yiosy <150297399+Yiosy@users.noreply.github.com>
2023-12-09 17:41:01 +0100

    basic calculations now work

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let firstNumber;
 let secondNumber;
 let operator;
+let isResult = true;
 
 function add(a, b) {
   return a + b;
@@ -30,35 +31,88 @@ function operate(operator, firstArgument, secondArgument) {
       break;
   }
 }
+
+function Values(x) {
+  {
+    if (firstNumber == null) {
+      firstNumber = Number(input.value);
+      operator = x;
+      input.value = "";
+    } else if (
+      typeof firstNumber == "number" &&
+      typeof secondNumber !== "number"
+    ) {
+      operator = x;
+      secondNumber = Number(input.value);
+      secondNumber = null;
+    } else {
+      operator = x;
+    }
+  }
+}
+
 const input = document.querySelector("input");
 const clearButton = document.querySelector(".clearButton");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const zero = document.getElementById("0");
 const operatorAdd = document.getElementById("add");
+const operatorSubstract = document.getElementById("subtract");
+const operatorMultiply = document.getElementById("multiply");
+const operatorDivide = document.getElementById("divide");
 const equals = document.getElementById("equals");
+
 numbers.forEach((x, i) =>
-  x.addEventListener("click", () => (input.value = `${input.value}${i + 1}`))
+  x.addEventListener("click", () => {
+    if (isResult == true) {
+      isResult = "false";
+      input.value = null;
+      secondNumber = null;
+      input.value = `${i + 1}`;
+    } else {
+      input.value = `${input.value}${i + 1}`;
+    }
+  })
 );
 
-clearButton.addEventListener("click", () => (input.value = ""));
+clearButton.addEventListener(
+  "click",
+  () => (
+    (input.value = null),
+    (operator = null),
+    (firstNumber = null),
+    (secondNumber = null)
+  )
+);
+
 zero.addEventListener("click", () => (input.value = input.value + "0"));
+
 operatorAdd.addEventListener("click", () => {
-  if (firstNumber == null) {
-    firstNumber = Number(input.value);
-    operator = "+";
-    input.value = "";
-  } else if (typeof firstNumber == "number") {
-    secondNumber = Number(input.value);
-    input.value = `${firstNumber}${operator}${secondNumber}`;
-  }
+  Values("+");
 });
+
+operatorSubstract.addEventListener("click", () => {
+  Values("-");
+});
+
+operatorMultiply.addEventListener("click", () => {
+  Values("*");
+});
+
+operatorDivide.addEventListener("click", () => {
+  Values("/");
+});
+
 equals.addEventListener("click", () => {
   if (typeof firstNumber == "number" && typeof secondNumber == "number") {
     input.value = operate(operator, firstNumber, secondNumber);
     firstNumber = Number(input.value);
-  } else if (typeof firstNumber == "number" && secondNumber == null) {
+
+    isResult = true;
+  } else if (typeof firstNumber == "number" && secondNumber !== "number") {
     secondNumber = Number(input.value);
     input.value = operate(operator, firstNumber, secondNumber);
+    firstNumber = Number(input.value);
+    isResult = true;
   }
 });
